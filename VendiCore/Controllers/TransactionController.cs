b9f6c8fee7dc4b8d37389428e65f53584eb092fd
Nyi Lynn <54408043+liner774@users.noreply.github.com>
2024-08-03 05:46:56 +0630

    transactin page done

---
+++ VendiCore/Controllers/TransactionController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VendiCore.Data;
+using VendiCore.Models;
+
+namespace VendiCore.Controllers
+{
+    public class TransactionController : Controller
+    {
+        private readonly VendingMachineContext _context;
+
+        public TransactionController(VendingMachineContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var transactions = await _context.Transactions
+                .Include(t => t.Products)
+                .Include(t => t.User)
+                .ToListAsync();
+
+            return View(transactions);
+        }
+    }
+}
